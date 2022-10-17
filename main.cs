85894c2ff66f81486455ff33d979ec9773ee7e91
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-17 18:32:14 +0000

    changes whitespace

--- main.cs
+++ main.cs
@@ -5,9 +5,9 @@
 
 using System;
 
-class Program 
+class Program
 {
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
         // This function accepts user input
         int height;
